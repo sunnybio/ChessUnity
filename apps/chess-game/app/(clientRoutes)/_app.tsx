4880a80c7290c548fc19a3101b296f7dc97708fe
sunnybio <wdeepeshsharma@gmail.com>
2023-11-19 04:18:15 +0530

    feat: google auth working, init credential login

--- apps/chess-game/app/(clientRoutes)/_app.tsx
+++ apps/chess-game/app/(clientRoutes)/_app.tsx
@@ -1,4 +1,6 @@
 import { SessionProvider } from "next-auth/react";
+
+
 export default function App({
     Component,
     pageProps: { session, ...pageProps },
--- apps/chess-game/app/(clientRoutes)/page.tsx
+++ apps/chess-game/app/(clientRoutes)/page.tsx
@@ -1,14 +1,45 @@
+import { getServerSession } from "next-auth/next";
+import Link from "next/link";
+import { ReactNode } from "react";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
-import { Board } from "ui/components";
+type Repo = {
+    name: string;
+    stargazers_count: number;
+};
+export default async function Home(): Promise<ReactNode> {
+    const session = await getServerSession(authOptions);
 
-export default function Home(): JSX.Element {
+    console.log("in page", session);
+    if (session) {
+        return (
+            <div className="w-full md:w-full xl:w-full">
+                <div className="flex justify-between">
+                    signed in as {session.user.name}, {session.user.email}
+                    <br />
+                    <button
+                        className="inline-block rounded bg-black px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                        type="button"
+                    >
+                        <Link href="/api/auth/signout">SignOut</Link>
+                    </button>
+                </div>
+            </div>
+        );
+    }
 
     return (
-    <div className="w-full md:w-full xl:w-full">
-       				 <div className="flex justify-center"> 
-            <Board />
-
+        <div className="w-full md:w-full xl:w-full">
+            <div className="flex justify-between">
+                Not signed in <br />
+                <br />
+                <button
+                    className="inline-block rounded bg-black px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                    type="button"
+                >
+                    <Link href="/api/auth/signin">Sign In</Link>
+                </button>
+            </div>
         </div>
-</div>
-    )
+    );
 }
