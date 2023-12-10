741d13b8b2b6ffcbe8f7815e3bc8e30b0d6427e8
sunnybio <wdeepeshsharma@gmail.com>
2023-12-10 18:36:24 +0530

    feat: init websocket server

--- apps/chess-game/app/(clientRoutes)/game/[room]/page.tsx
+++ apps/chess-game/app/(clientRoutes)/game/[room]/page.tsx
@@ -4,21 +4,25 @@ import { Chess } from "chess.ts";
 import { useEffect, useState } from "react";
 const GameRoom = (): JSX.Element => {
   const [position, setPostion] = useState("start");
+  const [msgList, setmsgList] = useState(["hello", "world"]);
   console.log("position", position);
   useEffect(() => {
     const game = new Chess();
     let move = game.moves()[3];
     game.move(move);
-    console.log("move", move);
-    console.log("new position", game.fen().toString());
     const timer = setTimeout(() => {
-      setPostion(`${game.fen()}`);
-    }, 5000);
+      setPostion(game.fen());
+    }, 1000);
   }, []);
   if (typeof window !== "undefined") {
     return (
-      <div className="flex rounded justify-center items-center bg-black h-full">
-        <Chessboard position={position} />
+      <div className="grid grid-cols-4 h-screen w-full bg-black">
+        <div className="col-span-1 bg-red-50">
+          <div className="block">{msgList}</div>
+        </div>
+        <div className="col-span-3 mx-auto">
+          <Chessboard position={position} />
+        </div>
       </div>
     );
   }
--- packages/db/src/mongodb/Models/Users.ts
+++ packages/db/src/mongodb/Models/Users.ts
@@ -10,5 +10,5 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
 });
 
-const User = mongoose.model.users || mongoose.model("users", userSchema);
+const User = mongoose.models.users || mongoose.model("users", userSchema);
 export default User;
