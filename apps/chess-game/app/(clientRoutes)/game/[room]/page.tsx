e6f448d37c4c44e5c90e3cac9a02eae6b38b52df
sunnybio <wdeepeshsharma@gmail.com>
2023-12-13 01:44:40 +0530

    feat: websocket connection working with chat

--- apps/chess-game/app/(clientRoutes)/game/[room]/page.tsx
+++ apps/chess-game/app/(clientRoutes)/game/[room]/page.tsx
@@ -1,39 +1,66 @@
-"use client";
-import Chessboard from "chessboardjsx";
-import { Chess } from "chess.ts";
-import { useEffect, useState } from "react";
+'use client'
+import Chessboard from 'chessboardjsx'
+import { Chess } from 'chess.ts'
+import { useEffect, useState } from 'react'
 
+const ws = new WebSocket('ws://localhost:8080')
 const GameRoom = (): JSX.Element => {
-  const [position, setPostion] = useState("start");
-  const [msgList, setmsgList] = useState(["hello", "world"]);
-  console.log("position", position);
-  const ws = new WebSocket("ws://localhost:8089");
-  ws.addEventListener("open", (event) => {
-    ws.send("HelloServer");
-  });
-  ws.addEventListener("message", (event: any) => {
-    console.log(event.data);
-  });
-  useEffect(() => {
-    const game = new Chess();
-    let move = game.moves()[3];
-    game.move(move);
-    const timer = setTimeout(() => {
-      setPostion(game.fen());
-    }, 1000);
-  }, []);
-  if (typeof window !== "undefined") {
-    return (
-      <div className="grid grid-cols-4 h-screen w-full bg-black">
-        <div className="col-span-1 bg-red-50">
-          <div className="block">{msgList}</div>
-        </div>
-        <div className="col-span-3 mx-auto">
-          <Chessboard position={position} />
-        </div>
-      </div>
-    );
-  }
-};
+    const [position, setPostion] = useState('start')
+    const [msgList, setmsgList] = useState(['hello'])
+    const [msg, setMsg] = useState('')
+    ws.onmessage = (event) => {
+        if (event.data instanceof Blob) {
+            const reader = new FileReader()
 
-export default GameRoom;
+            reader.onload = () => {
+                const newMessages = [...msgList, reader.result as string]
+                setmsgList(newMessages)
+            }
+
+            reader.readAsText(event.data)
+        } else {
+            const newMessages = [...msgList, event.data]
+            setmsgList(newMessages)
+        }
+    }
+    useEffect(() => {
+        const game = new Chess()
+        let move = game.moves()[3]
+        game.move(move)
+        const timer = setTimeout(() => {
+            setPostion(game.fen())
+        }, 1000)
+    }, [])
+
+    const sendMsg = async (e: any) => {
+        ws.send(msg)
+        setMsg('')
+    }
+    if (typeof window !== 'undefined') {
+        return (
+            <div className="grid grid-cols-4 h-screen w-full bg-black">
+                <div className="col-span-1 bg-gray-400">
+                    <div className="block h-1/3 bg-zinc-500 rounded-l ">
+                        {msgList.map((message, index) => (
+                            <div key={index}>{message}</div>
+                        ))}
+                    </div>
+                    <input
+                        className="p-1 w-full"
+                        type="text"
+                        name="message"
+                        value={msg}
+                        onChange={(e) => setMsg(e.target.value)}
+                    />
+                    <button onClick={sendMsg}>Send</button>
+                </div>
+                <div className="col-span-3 mx-auto">
+                    <Chessboard position={position} />
+                </div>
+            </div>
+        )
+    }
+    return <div>window not connected</div>
+}
+
+export default GameRoom
--- apps/game-server/index.ts
+++ apps/game-server/index.ts
@@ -1,15 +1,13 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer } from 'ws'
 
-const wss = new WebSocketServer({ port: 8089 });
+const wss = new WebSocketServer({ port: 8080 })
 
-wss.on("connection", function connection(ws: any) {
-  ws.on("message", function message(data: string) {
-    console.log("received: %sasdasd", data);
-    ws.send("something");
-    wss.clients.forEach((element: any) => {
-      console.log(element);
-    });
-  });
-
-  ws.send("something");
-});
+wss.on('connection', async function connection(ws: any) {
+    ws.on('message', function message(data: string) {
+        console.log('received: %sasdasd', data)
+        ws.send(data)
+        // wss.clients.forEach((element: any) => {
+        //     console.log(element)
+        // })
+    })
+})
